c9ca785ccaa0c8eba4c846ce42f0cfffe6d9d72a
VTIvanov20 <VTIvanov20@codingburgas.bg>
2026-05-28 14:09:13 +0200

    feat: add dataset with correct encoding

---
+++ dataset-analysis.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(readr)
+
+dataset <- read_csv(
+  "/Users/valeryivanov/Desktop/IMC Krems/Data Analysis/data-analysis-deliverable/sales_data_sample.csv",
+  locale = locale(encoding = "latin1")
+  )
+
+View(dataset)
