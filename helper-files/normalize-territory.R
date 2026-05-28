aed35f5514e923c17759819ffe537a2006429e6d
VTIvanov20 <VTIvanov20@codingburgas.bg>
2026-05-29 01:59:08 +0200

    feat: add dataset for APAC region & normalize

---
+++ helper-files/normalize-territory.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+
+# Data cleanup for TERRITORY
+# !! IMPORTANT: make sure to run the lines below ONLY AFTER the dataset has been initialized in the main file !!
+# We did this to take advantage of RStudio's environment, so you there's no need to call functions from separate files.
+
+# made a small dataset with all the countries in the APAC region
+# used for reference as seen below:
+apac_dataset <- read_csv(
+  "/Users/valeryivanov/Desktop/IMC Krems/Data Analysis/data-analysis-deliverable/data/apac_countries.csv",
+)
+
+# I had previously done this with just filtering, but forgot that all other entries would get overriden, thus leaving only APAC
+apac_countries <- apac_dataset$Country
+
+dataset <- dataset %>% 
+  mutate(
+    TERRITORY = if_else(
+      COUNTRY %in% apac_countries,
+      "APAC",
+      TERRITORY
+    )
+  )
+
+# for convenience again:
+# View(dataset)
