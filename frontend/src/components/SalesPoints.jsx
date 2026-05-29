9f191bd1cc8fdf3f5764f9253f0d23f32e5e28cf
VTIvanov20 <VTIvanov20@codingburgas.bg>
2026-05-29 21:15:43 +0200

    feat: update yarn and salespoints

--- frontend/src/components/SalesPoints.jsx
+++ frontend/src/components/SalesPoints.jsx
@@ -2,6 +2,17 @@ import * as THREE from 'three';
 import { useRoute } from 'wouter';
 import { latLonToVector3 } from '../util/coordinateHelper';
 
+// I made some colors
+const TERRITORY_COLORS = {
+    NA: '#00B8FA',      // blue
+    EMEA: '#FF9512',    // orange
+    APAC: '#46E15F'     // green
+};
+
+function getTerritoryColor(territory) {
+    return TERRITORY_COLORS[String(territory || '').toUpperCase()] || '#ff4d9d';
+}
+
 export function SalesPoints({ data, onPointClick }) {
     const [matchStart] = useRoute('/start');
 
@@ -29,6 +40,8 @@ export function SalesPoints({ data, onPointClick }) {
                     0.09
                 );
 
+                const markerColor = getTerritoryColor(item.TERRITORY);
+
                 return (
                     <mesh
                         key={`${item.CITY}-${item.COUNTRY}-${index}`}
@@ -43,9 +56,9 @@ export function SalesPoints({ data, onPointClick }) {
                             });
                         }}
                     >
-                        <sphereGeometry args={[1, 16, 16]} />
+                        <sphereGeometry args={[0.5, 16, 16]} />
                         <meshBasicMaterial
-                            color="#ff4d9d"
+                            color={markerColor}
                             transparent
                             opacity={0.95}
                             depthWrite={false}
